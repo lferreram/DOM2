85fbf56349e37bcd7e3b50166dffa0386ec6abba
lferreram <lferreram@yahoo.com>
2023-05-28 19:30:38 -0600

    first commit

---
+++ DOM2.js
@@ -0,0 +1,17 @@
+const elemento1 = document.getElementById("Input para nombre")
+console.log(elemento1);
+
+const elemento2 =document.getElementById("Input para email")
+console.log(elemento2);
+
+const elemento3 =document.getElementById('Ingreso de datos')
+elemento3.style.color="#CC66CC";
+console.log(elemento3);
+
+const elemento4=document.getElementById("Creado por:Larissa Ferrera");
+elemento4.style.color="#660066";
+console.log(elemento4);
+
+
+
+
